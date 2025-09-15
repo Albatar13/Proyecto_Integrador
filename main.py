eace47ff078192615835441a12868f3bb56f7980
Albatar13 <164494860+Albatar13@users.noreply.github.com>
2025-09-15 19:49:59 +0200

    Update funciones + add borrar() y buscar() main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-libros = []
-
 def anadir_libro():
     tab = []
     tab.append(input("Ingresar el nombre del libro"))
     tab.append(input("Ingresar el nombre del autor"))
-    tab.append(input("Ingresar la fecha de publicacion"))
-    tab.append(input("Ingresar el  numero de libros disponible"))
+    tab.append(int(input("Ingresar la fecha de publicacion")))
+    tab.append(int(input("Ingresar el  numero de libros disponible")))
     print(tab)
     libros.append(tab)
 
 def editar_libro():
     nombre = input("Ingresar el nombre del libro que quieres editar")
     for libro in libros:
-        if nombre == libro[0]:
+        if nombre.lower() == libro[0].lower():
             while True: 
                 num = int(input("Que quieres cambiar : 1 = Cambiar nombre / 2 = Cambiar autor / 3 = Cambiar fecha publicacion / 4= Cambiar numero disponible"))
                 match num :
@@ -23,14 +21,55 @@ def editar_libro():
                         libro[1] = input("Ingresar el nuevo autor")
                         break
                     case 3:
-                        libro[2] = input("Ingresar la nueva fecha")
+                        libro[2] = int(input("Ingresar la nueva fecha"))
                         break
                     case 4: 
-                        libro[3] = input("Ingresar el nuevo numero de libros disponible")
+                        libro[3] = int(input("Ingresar el nuevo numero de libros disponible"))
                         break
                     case _:
                         print("Error ingrese un numero entre 1 y 4 ")
-            break
+            break   
     else : 
         print("Livre no encontrado en la lista")
+
+def borrar_libro():
+    nombre = input("Ingresar el nombre del libro que quieres borrar")
+    for libro in libros:
+        if nombre.lower() == libro[0].lower():
+            libros.remove(libro)
+            break
+    else:
+        print("Libro no encontrado en la lista ")
         
+def buscar_libro():
+    num = int(input("Quieres buscar con el nombre : ingresa 1 o con el autor : ingresa 2"))
+    match num:
+        case 1:
+            nombre = input("Ingresar el nombre del libro que quieres buscar")
+            for libro in libros:
+                if nombre.lower() == libro[0].lower():
+                    print("Libro encontrado sus informaciones son :")
+                    print(libro)
+                    break
+            else:
+                print("Libro no encontrado en la lista ")
+        case 2:
+            autor = input("Ingresar el nombre del autor que quieres buscar")
+            numlibrosautor = 0
+            librosautor = []
+            for libro in libros:
+                if autor.lower() == libro[1].lower():
+                    librosautor.append(libro)
+                    numlibrosautor +=1
+            if numlibrosautor == 0:
+                print("Autor no encontrado en la lista ")
+            else:
+                print(f"Autor encontrado, tenemos {numlibrosautor} de el que son")
+                for libro in librosautor:
+                    print(libro)
+        case _:
+            print("Error no ha ingresado un numero entre 1 y 2")
+            
+            
+                
+                    
